92888ce2062ad4b54e832631121569cd985b8ba7
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-02-14 09:11:21 +0300

    Add button

--- src/Book.js
+++ src/Book.js
@@ -3,23 +3,46 @@ import React, { Component } from 'react';
 
 
 class Book extends Component {
+    state = {
+        hide:true
+    }
+    hideMeme=() => 
+        { this.setState({hide:!this.state.hide })
+    
+    }
   render() {
+    let Hide='';
 
-    return (
-        <div className>
-         <p> {this.props.bookData.isbn}</p>
-         <p>{this.props.bookData.title} </p>
-         <p>{this.props.bookData.subtitle}</p>
-         <p>{this.props.bookData.author}</p>
-         <p>{this.props.bookData.published}</p>
-         <p>{this.props.bookData.publisher}</p>
-         <p>{this.props.bookData.pages}</p>
-         <p>{this.props.bookData.description}</p>
-         <a href={this.props.bookData.website}> website </a> 
-         
-          
-        </div>
-      );
+    if(this.state.hide === true){
+      Hide = <p>{this.props.bookData.subtitle}</p>
+      
+      
+    }
+    if(this.state.hide === true){
+        return (
+            <div>
+                <h1 onClick={this.hideMeme}>{this.props.bookData.title} </h1>
+                <h1>Nothing to show...</h1>
+            </div>
+        );
+    } else {
+        return (
+            <div className>
+             <p> {this.props.bookData.isbn}</p>
+             <h1 onClick={this.hideMeme}>{this.props.bookData.title} </h1>
+             <p>{this.props.bookData.subtitle}</p>
+             <p>{this.props.bookData.author}</p>
+             <p>{this.props.bookData.published}</p>
+             <p>{this.props.bookData.publisher}</p>
+             <p>{this.props.bookData.pages}</p>
+             <p>{this.props.bookData.description}</p>
+             <a href={this.props.bookData.website}> website </a> 
+             
+              {Hide}
+            </div>
+          );
+    }
+
     }
   }
   
